b8d6142dfeda0f6581f3904fc409da39dc891cf0
sumanth <mrsumanth1947@gmail.com>
2025-03-23 11:30:14 +0530

    prisma setup done

---
+++ prisma/migrations/20250322084728_make_profileimage_not_required/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "users" ALTER COLUMN "profile_img_url" DROP NOT NULL;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
+import { prismaClient } from "./lib/db";
 
 const startServer = async () => {
   const app = express();
@@ -11,12 +12,36 @@ const startServer = async () => {
   const gqlServer = new ApolloServer({
     typeDefs: `
         type Query {
-            hello: String!
+          hello: String!
+        }
+        
+        type Mutation {
+          createUser(
+            firstName: String!
+            lastName: String!
+            email: String!
+            password: String!
+          ): Boolean
         }
     `,
     resolvers: {
       Query: {
         hello: () => `hello im graphql`,
+      },
+
+      Mutation: {
+        createUser: async (_, { firstName, lastName, email, password }: { firstName: string; lastName: string; email: string; password: string }) => {
+          await prismaClient.user.create({
+            data: {
+              firstName,
+              lastName,
+              email,
+              password,
+              salt: "random",
+            },
+          });
+          return true;
+        },
       },
     },
   });
